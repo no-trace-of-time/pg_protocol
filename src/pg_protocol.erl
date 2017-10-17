a5b2c3589b7ae17408b9fb4952bfab14e6b8365e
simonxu72 <simon.xu72@gmail.com>
2017-10-17 14:11:27 +0800

    in_2_out/2/3 pass ut

--- src/pg_protocol.erl
+++ src/pg_protocol.erl
@@ -1,12 +1,22 @@
 -module(pg_protocol).
 -include_lib("eunit/include/eunit.hrl").
+-behavior(pg_model).
 
 %% callbacks
 -callback in_2_out_map() -> map().
+
 %% API exports
 -export([
   out_2_in/2
+  , in_2_out/2
+  , in_2_out/3
 ]).
+
+-export([
+
+]).
+
+-define(TEST_PROTOCOL, pg_test_utils:name(protocol)).
 
 %%====================================================================
 %% API functions
@@ -29,13 +39,31 @@ out_2_in(M, PL) when is_atom(M), is_list(PL) ->
   {VL, _, _} = lists:foldl(F, {[], Model2OutMap, PL}, Fields),
   pg_model:new(M, VL).
 
+%%----------------------------------------------
+-spec in_2_out(M, Model, Format) -> WireFormat when
+  M :: atom(),
+  Model :: tuple(),
+  Format :: proplists | binary |string,
+  WireFormat :: proplists:proplist() | binary().
+
+in_2_out(M, Model, proplists) ->
+  do_in_2_out(M, Model);
+in_2_out(M, Model, binary) ->
+  VL = do_in_2_out(M, Model),
+  xfutils:post_vals_to_iolist(VL);
+in_2_out(M, Model, string) ->
+  in_2_out(M, Model, binary).
+
+in_2_out(M, Model) ->
+  in_2_out(M, Model, proplists).
+
+do_in_2_out(M, Model) when is_atom(M), is_tuple(Model) ->
+  [in_2_out_one_field(M, Model, Field) || Field <- pg_model:fields(M)].
+
 
 %%====================================================================
 %% Internal functions
 %%====================================================================
-get_out_fields(M) when is_atom(M) ->
-  [signature | M: sign_fields()].
-
 out_2_in_one_field(Field, {Acc, Model2OutMap, PL}) when is_atom(Field), is_list(Acc), is_map(Model2OutMap) ->
   Config = maps:get(Field, Model2OutMap),
 
@@ -64,6 +92,34 @@ do_out_2_in_one_field({KeyInPL, integer}, PL) when is_binary(KeyInPL), is_list(P
   end;
 do_out_2_in_one_field(KeyInPL, PL) when is_binary(KeyInPL), is_list(PL) ->
   proplists:get_value(KeyInPL, PL).
+
+
+%%----------------------------------------------------------
+field_config(M, Field) when is_atom(M), is_atom(Field) ->
+  ConfigMap = M:in_2_out_map(),
+  maps:get(Field, ConfigMap).
+
+in_2_out_one_field(M, Model, Field) when is_atom(M), is_tuple(Model), is_atom(Field) ->
+  Config = field_config(M, Field),
+  do_in_2_out_one_field(M, Model, {Field, Config}).
+
+do_in_2_out_one_field(M, Model, {Field, FieldInPVBIN}) when is_atom(Field), is_binary(FieldInPVBIN) ->
+  Value = pg_model:get(M, Model, Field),
+  {FieldInPVBIN, Value};
+do_in_2_out_one_field(M, Model, {Field, {FieldInPVBIN, integer}}) when is_atom(Field), is_binary(FieldInPVBIN) ->
+  ValueInteger = pg_model:get(M, Model, Field),
+  ValueBin = integer_to_binary(ValueInteger),
+  {FieldInPVBIN, ValueBin}.
+
+do_out_2_in_one_filed_test() ->
+  M = ?TEST_PROTOCOL,
+  Protocol = pg_model:new(M, [{txnAmt, 9997}, {settleAmt, 9998}, {txnType, <<"01">>}]),
+
+  ?assertEqual({<<"txnAmt">>, <<"9997">>}, in_2_out_one_field(M, Protocol, txnAmt)),
+  ?assertEqual({<<"settleAmt">>, <<"9998">>}, in_2_out_one_field(M, Protocol, settleAmt)),
+  ?assertEqual({<<"txnType">>, <<"01">>}, in_2_out_one_field(M, Protocol, txnType)),
+
+  ok.
 
 %%====================================================================
 %% Internal Test functions
--- test/t_pg_protocol.erl
+++ test/t_pg_protocol.erl
@@ -15,8 +15,9 @@
 
 
 
+-define(TEST_PROTOCOL, pg_test_utils:name(protocol)).
 
-out_2_model_test() ->
+postvals() ->
   PL = [
     {<<"accessType">>, <<"0">>}
     , {<<"bizType">>, <<"000201">>}
@@ -45,15 +46,18 @@ out_2_model_test() ->
     , {<<"accNo">>, <<"95555">>}
 
 
-    %% extra null fields
+%% extra null fields
     , {<<"Fld1">>, <<"aaa">>}
     , {<<"Fld2">>, <<"aaa">>}
-  ],
+  ].
+
+out_2_in_test() ->
+  PL = postvals(),
 
   ExcludeFields = [<<"Fld1">>, <<"Fld2">>],
   R = [{binary_to_atom(KeyBin, utf8), Value} || {KeyBin, Value} <- PL, not  lists:member(KeyBin, ExcludeFields)],
 
-  M = pg_test_utils:name(protocol),
+  M = ?TEST_PROTOCOL,
 
   ModelUpRespPay = pg_protocol:out_2_in(M, PL),
 
@@ -65,3 +69,50 @@ out_2_model_test() ->
 
   ?assertEqual(ModelUpRespPay, ModelExpected),
   ok.
+
+%%--------------------------------------------------------------------
+
+in_2_out_test() ->
+  M = ?TEST_PROTOCOL,
+  PL = postvals(),
+
+  Protocol = pg_protocol:out_2_in(M, PL),
+
+  PLOut = [
+    {<<"version">>, <<"5.0.0">>},
+    {<<"encoding">>, <<"UTF-8">>},
+    {<<"certId">>, <<"9">>},
+    {<<"signature">>, <<"sig">>},
+    {<<"signMethod">>, <<"01">>},
+    {<<"txnType">>, <<"01">>},
+    {<<"txnSubType">>, <<"01">>},
+    {<<"bizType">>, <<"000201">>},
+    {<<"accessType">>, <<"0">>},
+    {<<"merId">>, <<"012345678901234">>},
+    {<<"orderId">>, <<"20161111">>},
+    {<<"txnTime">>, <<"19991212121212">>},
+    {<<"txnAmt">>, <<"100">>},
+    {<<"currencyCode">>, <<"156">>},
+    {<<"reqReserved">>, <<"reqR">>},
+    {<<"reserved">>, <<"reserved">>},
+    {<<"accNo">>, <<"95555">>},
+    {<<"queryId">>, <<"20162222">>},
+    {<<"respCode">>, <<"00">>},
+    {<<"respMsg">>, <<"success">>},
+    {<<"settleAmt">>, <<"100">>},
+    {<<"settleCurrencyCode">>, <<"156">>},
+    {<<"settleDate">>, <<"20163333">>},
+    {<<"traceNo">>, <<"22222">>},
+    {<<"traceTime">>, <<"101010">>},
+    {<<"exchangeRate">>, undefined}
+  ],
+
+  ?assertEqual(PLOut, pg_protocol:in_2_out(M, Protocol)),
+
+  PLOutString = <<"version=5.0.0&encoding=UTF-8&certId=9&signature=sig&signMethod=01&txnType=01&txnSubType=01&bizType=000201&accessType=0&merId=012345678901234&orderId=20161111&txnTime=19991212121212&txnAmt=100&currencyCode=156&reqReserved=reqR&reserved=reserved&accNo=95555&queryId=20162222&respCode=00&respMsg=success&settleAmt=100&settleCurrencyCode=156&settleDate=20163333&traceNo=22222&traceTime=101010">>,
+  ?assertEqual(PLOutString, list_to_binary(pg_protocol:in_2_out(M, Protocol, binary))),
+  ?assertEqual(PLOutString, list_to_binary(pg_protocol:in_2_out(M, Protocol, string))),
+
+  ok.
+
+
